a1c5379140fb51147a332f4ddfafea8343f0a0ad
Arnabpahari <156195450+Arnabpahari@users.noreply.github.com>
2025-02-12 22:50:37 +0530

    Update views.py

--- DjangoProject1/userdata/views.py
+++ DjangoProject1/userdata/views.py
@@ -16,7 +16,7 @@ def predict(request):
     return render(request,'predict.html')
 
 def result(request):
-    data=pd.read_csv(r'C:\Users\arnab\Downloads\diabetes.csv')
+    data=pd.read_csv("DjangoProject1/static/DiabetesPrediction/images/diabetes.csv")
 
     X=data.drop("Outcome", axis=1)
     Y=data["Outcome"]
